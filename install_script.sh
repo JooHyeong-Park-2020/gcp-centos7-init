0cbbb66a5221057418c2b4a339798689c2ccde57
JOOHYEONG-PARK <reverse0092@gmail.com>
2019-08-17 11:48:12 +0900

    커밋

--- install_script.sh
+++ install_script.sh
@@ -1,1 +1,4 @@
-curl https://raw.githubusercontent.com/JooHyeong-Park-2020/gcp-centos7-init/master/start.sh >> start.sh && chmod 700 ./start.sh && ./start.sh &
+curl https://raw.githubusercontent.com/JooHyeong-Park-2020/gcp-centos7-init/master/start.sh \
+    >> start.sh && \
+    chmod 700 ./start.sh && \
+    ./start.sh &
--- start.-all.sh
+++ start.-all.sh
@@ -2447,4 +2447,4 @@ chown -R ${NGINX_USER}:${NGINX_USER_GROUP} \
 chown -R ${NGINX_USER}:${NGINX_USER_GROUP} \
    ${NGINX_WEBDAV_CLIENT_BODY_TEMP_PATH}
 
-# reboot
+reboot
