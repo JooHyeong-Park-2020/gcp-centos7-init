a3015b8272661ccb9dcc0a33d60571eced712da6
JooHyeong-Park-2020 <reverse.jh2020@gmail.com>
2019-09-07 23:32:12 +0900

    커밋

--- install_script.sh
+++ install_script.sh
@@ -1,5 +1,6 @@
 export GIT_REPO_PATH=https://raw.githubusercontent.com/JooHyeong-Park-2020/gcp-centos7-init/master && \
 export TEMP_PATH=/tmp && \
+export SETTING_INFO_DIRECTORY_PATH=_centos7_settingInfo && \
 export SETTING_INFO_NAME=settingInfo && \
 curl ${GIT_REPO_PATH}/start.sh \
     > start.sh && \
