d4fbba06908cf804cef655192b4e3946a2118a02
JooHyeong-Park-2020 <reverse.jh2020@gmail.com>
2019-09-07 22:37:13 +0900

    커밋

--- start.sh
+++ start.sh
@@ -13,16 +13,13 @@ cd ${TEMP_PATH}
 
 ######################################
 
-executeScript()
+function executeScript()
 {
     local scriptName=$1
 
     echo "$workName  $scriptName  실행"
 
-    curl ${GIT_REPO_PATH}/${workName}/${scriptName}.sh \
-        > ${TEMP_PATH}/${workName}/${scriptName}.sh
-    chmod 700 ${TEMP_PATH}/${workName}/${scriptName}.sh
-    ${TEMP_PATH}/${workName}/${scriptName}.sh
+
 
 }
 
@@ -38,7 +35,11 @@ for workName in ${workList[@]}; do
     for scriptName in $scriptList; do
 
         echo "-----"$scriptName" 실행-----"
-        executeScript $scriptName
+        
+        curl ${GIT_REPO_PATH}/${workName}/${scriptName}.sh \
+            > ${TEMP_PATH}/${workName}/${scriptName}.sh
+        chmod 700 ${TEMP_PATH}/${workName}/${scriptName}.sh
+        ${TEMP_PATH}/${workName}/${scriptName}.sh
 
     done
 
